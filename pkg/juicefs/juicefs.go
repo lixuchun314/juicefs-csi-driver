c23248842dbcdc5647ee839bd0d160cc60ab2ff1
leo.li <leo.li@vectbase.com>
2021-12-23 10:01:18 +0800

    Configure 'trash-days' in format

--- pkg/juicefs/juicefs.go
+++ pkg/juicefs/juicefs.go
@@ -429,6 +429,7 @@ func (j *juicefs) ceFormat(secrets map[string]string, noUpdate bool, extraEnvs m
 		"access-key",
 		"block-size",
 		"compress",
+		"trash-days",
 	}
 	keysStripped := []string{"secret-key"}
 	isOptional := map[string]bool{
